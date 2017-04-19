cb23fea4b5d2a8fcca344940d85815005a8023f9
Judson <nyarly@gmail.com>
2017-04-18 21:52:35 -0700

    Whitespace trimming

--- model.js
+++ model.js
@@ -5,16 +5,16 @@ function Clock(name) {
 	if (name == undefined) {name = 'Clock #' + clocks.length;};
 
 	this.name = name;
-	
+
 	this.format = 'classic';
-	
+
 	this.priority = 'active';
-	
+
 	this.fillColor = 'black';
 	this.emptyColor = 'white';
 
 	this.currentSegment = 0;
-	
+
 	this.labels = [
 		"Noon",
 		"3 o'clock",
@@ -24,23 +24,23 @@ function Clock(name) {
 		"11 o'clock",
 		"midnight!"
 	];
-	
+
 	this.advance = function() {
 		this.currentSegment = Math.min(this.currentSegment + 1,this.labels.length-1)
 	};
-	
+
 	this.rewind = function() {
 		this.currentSegment = Math.max(this.currentSegment - 1,0)
 	};
-	
+
 	this.updateLabel = function(segment,newLabel) {
 		this.labels[segment] = newLabel;
 	};
-	
+
 	this.updateName = function(newName) {
 		this.name = newName;
 	};
-	
+
 	clocks.push(this);
-	
+
 };
