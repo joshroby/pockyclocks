814b3f381065f5437c97f573e941c19e88fd99f7
Judson <nyarly@gmail.com>
2017-04-19 10:27:44 -0700

    All static assets in public/

--- app/controllers/clocks_controller.rb
+++ app/controllers/clocks_controller.rb
@@ -2,7 +2,7 @@ class ClocksController < ApplicationController
   include ActionController::MimeResponds
   def index
     respond_to do |format|
-      format.html { render html: File.read(Rails.root + "index.html".to_s).html_safe}
+      format.html { render html: File.read(Rails.root + "public/index.html".to_s).html_safe}
     end
   end
 end
